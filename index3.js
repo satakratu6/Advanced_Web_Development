f79584e65109c0b886d05322fc049d2d37dfbacc
Satakratu Chakraborty <satakratu.madhu@gmail.com>
2024-02-17 10:40:35 +0530

    new

---
+++ index3.js
@@ -0,0 +1,6 @@
+const http= require('http');
+const server= http.createServer((req, res) => {
+    res.end('Hello, this is your Node.js server!');
+});
+const port=4000;
+server.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
