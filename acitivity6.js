ef5548bdb8605090a8343041cae5f5a54e483f03
Satakratu Chakraborty <satakratu.madhu@gmail.com>
2024-02-28 12:35:43 +0530

    new

---
+++ acitivity6.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const port = 3000;
+
+app.get('/', (req, res) => {
+    fs.readFile('form.html', 'utf-8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.end("Please check console");
+        } else {
+            res.send(data);
+        }
+    });
+});
+
+app.get('/submit', (req, res) => {
+    let data = req.query;
+    fs.writeFile('form.txt', JSON.stringify(data), 'utf-8', (err) => {
+        if (err) {
+            console.error(err);
+            res.end('Internal server error');
+            return;
+        } else {
+            res.send('Data saved successfully');
+        }
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
--- expressappl.js
+++ expressappl.js
@@ -1,9 +1,16 @@
 const express=require('express');
 const app=express();
+const fs=require('fs');
 const port=3000;
-app.get('/',(req,res)=>{
-    res.send('Hello World!');
+app.get('/readfile',(req,res)=>{
+    fs.readFile('example2.txt','utf-8',(err,data)=>{
+        if(err){
+            console.error(err);
+            return res.status(500).send("Erro reading the file");
+        }
+        res.send(data);
+    });
 });
 app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${port}`);
-})
+});
