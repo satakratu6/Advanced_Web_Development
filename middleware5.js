7bdbb8bab8c8ddf723556d64f2d513f8f1404a4b
satakratu6 <satakratu.madhu@gmail.com>
2024-04-03 10:08:20 +0530

    new

---
+++ middleware5.js
@@ -0,0 +1,4 @@
+//implement a basic login page using HTML for a web application
+//serve this login page using Express.js on the /login route.
+//create a middleware function for authentication in Express.js, which validates user credentials against a hardcoded database.
+//define a protected route 
